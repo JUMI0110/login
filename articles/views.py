90c026327682141310ad259b2a12fe0455cc99cc
jumi <ajsel422@gmail.com>
2024-08-06 14:43:23 +0900

    article read기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
+from .models import Article
 from .forms import ArticleForm
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    articles = Article.objects.all()
+    context = {
+        'articles': articles, 
+    }
+    return render(request, 'index.html', context)
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+
+    context = {
+        'article': article,
+    }
+    return render(request, 'detail.html', context)
 
 
 def create(request):
