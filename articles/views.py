b4b53b89d663aab76e517c2ed279505bef88d823
jumi <ajsel422@gmail.com>
2024-08-07 10:21:00 +0900

    Comment create 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Article
-from .forms import ArticleForm
+from .forms import ArticleForm, CommentForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -14,9 +14,11 @@ def index(request):
 @login_required
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
 
     context = {
         'article': article,
+        'form': form
     }
     return render(request, 'detail.html', context)
 
@@ -37,3 +39,25 @@ def create(request):
         'form': form,
     }
     return render(request, 'form.html', context)
+
+
+def comment_create(request, article_id):
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        
+        # 객체를 넣는 코드
+        # comment.user = request.user
+        # article = Article.objects.get(id=article_id)
+        # comment.article = article
+        
+        # id값을 넣는 코드 
+        comment.user_id = request.user.id
+        comment.article_id = article_id
+        
+        comment.save()
+        return redirect('articles:detail', id=article_id)
+
+
+    
